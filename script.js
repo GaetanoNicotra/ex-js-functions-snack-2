9e38b012659c4aa080a8587e70b8605cf4ad7ed9
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-07-14 19:10:03 +0200

    [UPD] Snack 2 - 3

--- script.js
+++ script.js
@@ -14,3 +14,18 @@ const somma = function (a, b) {
 const somma = (a, b) => {
     return a + b;
 }
+
+// SNACK 2
+
+const quadrato = (n) => n * n;
+
+
+// SNACK 3
+
+const sottrazione = (a, b) => a - b;
+const moltiplicazione = (a, b) => a * b;
+
+const eseguiOperazione = (a, b, operazione) => operazione(a, b)
+
+console.log(eseguiOperazione(60, 20, sottrazione))
+console.log(eseguiOperazione(10, 28, moltiplicazione))
