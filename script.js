d3bd95e05ed13e9c32fa08a3a51e0c7a3de62e3f
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-07-14 18:53:31 +0200

    [UPD] Snack 1

---
+++ script.js
@@ -0,0 +1,16 @@
+// SNACK 1
+
+// dichiarativa
+function somma(a, b) {
+    return a + b;
+}
+
+// anonima
+const somma = function (a, b) {
+    return a + b;
+}
+
+// arrow function
+const somma = (a, b) => {
+    return a + b;
+}
