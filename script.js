f941a6041ef04dccef6d42c72238986f4bdb6ffc
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-07-14 21:57:48 +0200

    [UPD] snack 10

--- script.js
+++ script.js
@@ -1,116 +1,116 @@
-// // SNACK 1
+// SNACK 1
 
-// // dichiarativa
-// function somma(a, b) {
-//     return a + b;
-// }
+// dichiarativa
+function somma(a, b) {
+    return a + b;
+}
 
-// // anonima
-// const somma = function (a, b) {
-//     return a + b;
-// }
+// anonima
+const somma = function (a, b) {
+    return a + b;
+}
 
-// // arrow function
-// const somma = (a, b) => {
-//     return a + b;
-// }
-
-
-// // SNACK 2
-
-// const quadrato = (n) => n * n;
+// arrow function
+const somma = (a, b) => {
+    return a + b;
+}
 
 
-// // SNACK 3
+// SNACK 2
 
-// const sottrazione = (a, b) => a - b;
-// const moltiplicazione = (a, b) => a * b;
-
-// const eseguiOperazione = (a, b, operazione) => operazione(a, b)
-
-// console.log(eseguiOperazione(60, 20, sottrazione))
-// console.log(eseguiOperazione(10, 28, moltiplicazione))
+const quadrato = (n) => n * n;
 
 
-// // SNACK 4
+// SNACK 3
 
-// const creaTimer = (ms) => {
-//     return function () {
-//         setTimeout(() => {
-//             console.log('TEMPO SCADUTO!')
-//         }, ms);
-//     };
-// };
+const sottrazione = (a, b) => a - b;
+const moltiplicazione = (a, b) => a * b;
 
-// const timer = creaTimer(2000);
-// timer()
+const eseguiOperazione = (a, b, operazione) => operazione(a, b)
+
+console.log(eseguiOperazione(60, 20, sottrazione))
+console.log(eseguiOperazione(10, 28, moltiplicazione))
 
 
-// // SNACK 5
+// SNACK 4
 
-// const stampaOgniSecondo = (mess) => {
-//     return setInterval(function () {
-//         console.log(mess)
-//     }, 1000)
-// }
+const creaTimer = (ms) => {
+    return function () {
+        setTimeout(() => {
+            console.log('TEMPO SCADUTO!')
+        }, ms);
+    };
+};
 
-// const messaggio = stampaOgniSecondo('Benvenuto in JavaScript')
+const timer = creaTimer(2000);
+timer()
 
 
-// //SNACK 6
+// SNACK 5
 
-// let counter = 0;
+const stampaOgniSecondo = (mess) => {
+    return setInterval(function () {
+        console.log(mess)
+    }, 1000)
+}
 
-// const creaContatoreAutomatico = (ms) => {
-//     return function () {
-//         setInterval(function () {
-//             counter = counter + 1;
-//             console.log(counter)
-//         }, ms)
-//     }
-// }
+const messaggio = stampaOgniSecondo('Benvenuto in JavaScript')
 
-// const valore = creaContatoreAutomatico(3000);
-// valore()
+
+//SNACK 6
+
+let counter = 0;
+
+const creaContatoreAutomatico = (ms) => {
+    return function () {
+        setInterval(function () {
+            counter = counter + 1;
+            console.log(counter)
+        }, ms)
+    }
+}
+
+const valore = creaContatoreAutomatico(3000);
+valore()
 
 
 // SNACK 7
 
-// const eseguiEferma = (mess, inizio, fine) => {
-//     return function () {
-//         const intervalloID = setInterval(() => {
-//             console.log(mess);
-//         }, inizio);
-//         setTimeout(() => {
-//             clearInterval(intervalloID);
-//         }, fine);
-//     };
-// };
+const eseguiEferma = (mess, inizio, fine) => {
+    return function () {
+        const intervalloID = setInterval(() => {
+            console.log(mess);
+        }, inizio);
+        setTimeout(() => {
+            clearInterval(intervalloID);
+        }, fine);
+    };
+};
 
-// const valori = eseguiEferma('Ciao Mondo!!', 2000, 10000);
-// valori();
+const valori = eseguiEferma('Ciao Mondo!!', 2000, 10000);
+valori();
 
 
 // SNACK 8
 
-// const contoAllaRovescia = (num) => {
-//     return function () {
-//         const timerValue = setInterval(function () {
-//             if (num != 0) {
-//                 num--;
-//                 console.log(num)
-//             }
-//             else {
-//                 clearInterval(timerValue)
-//                 console.log('Tempo scaduto!')
-//             }
+const contoAllaRovescia = (num) => {
+    return function () {
+        const timerValue = setInterval(function () {
+            if (num != 0) {
+                num--;
+                console.log(num)
+            }
+            else {
+                clearInterval(timerValue)
+                console.log('Tempo scaduto!')
+            }
 
-//         }, 1000)
-//     }
-// }
+        }, 1000)
+    }
+}
 
-// const numerino = contoAllaRovescia(12)
-// numerino()
+const numerino = contoAllaRovescia(12)
+numerino()
 
 
 // SNACK 9
@@ -129,3 +129,15 @@ sequenzaOperazioni([
     () => console.log("Operazione 2"),
     () => console.log("Operazione 3")
 ], 2000);
+
+
+// SNACK 10
+
+
+const creaThrottler = (funzione, tempo) => {
+    return setInterval(() => {
+        funzione()
+    }, tempo)
+};
+
+const throttledLog = creaThrottler(() => console.log("Eseguito!"), 2000);
